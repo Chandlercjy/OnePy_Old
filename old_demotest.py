e8397d88bd4953d3dfcbe6c05a064e7afa892639
chandler <chenjiayicjy@126.com>
2017-07-06 14:30:13 +0800

    add data

--- old_demotest.py
+++ old_demotest.py
@@ -1,3 +1,5 @@
+#coding=utf-8
+
 import talib.abstract as tb
 import pandas as pd
 import OnePy as op
@@ -25,19 +27,20 @@ class MyStrategy(op.Strategy):
 #                 self.exitall(s)
 #                 self.long(s)#,strength=2,percent=True,risky=True)
 
-
-data = op.csv_reader('/Users/chandler/Desktop/stock/data_csv', ['000428','000429'],start='2015-02-10')
+df = pd.read_csv('000001.csv', index_col=0, parse_dates=True)
+symbol_list = ['000001']
+data = op.DataFrame_reader(df, symbol_list,start='2015-02-10')
 strategy = MyStrategy(data,[5,15])
 portfolio = op.NaivePortfolio(data,initial_capital=200000)
 go = op.OnePiece(data, strategy, portfolio)
 
+
+go.print_trade()
+go.set_commission=2
+go.print_stats()
+go.sunny()
+
 # p_sma1=range(5,15)
 # p_sma2=range(20,30)
 # ppp = op.params_generator(p_sma1,p_sma2)
-
 # op.optimizer(MyStrategy,op.NaivePortfolio,data,ppp)
-
-go.print_trade()
-# go.set_commission=2
-go.print_stats()
-go.sunny()
